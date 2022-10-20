9f77b96b48d892fd6961ea3bd7f5a66292437703
kaz-koba <kazuki.kobayashi@insightedge.jp>
2022-10-20 10:57:11 +0900

    fix

--- complicated.js
+++ complicated.js
@@ -1,7 +1,6 @@
-protected void doGet(HttpServletRequest request, HttpServletResponse resp) {
-    String firstName = request.getParameter("firstName");
-    resp.getWriter().append("<div>");
-    resp.getWriter().append("Search for " + firstName);
-    resp.getWriter().append("</div>");
-}
-
+const doGet = (request, resp) => {
+  let firstName = request.getParameter("firstName");
+  resp.getWriter().append("<div>");
+  resp.getWriter().append("Search for " + firstName);
+  resp.getWriter().append("</div>");
+};
