a6b1520867c18fb53ce8e7b91d2e78c0c31e620e
kaz-koba <kazuki.kobayashi@insightedge.jp>
2022-10-24 13:50:43 +0900

    test

--- complicated.js
+++
@@ -1,6 +0,0 @@
-const doGet = (request, resp) => {
-  let firstName = request.getParameter("firstName");
-  resp.getWriter().append("<div>");
-  resp.getWriter().append("Search for " + firstName);
-  resp.getWriter().append("</div>");
-};
